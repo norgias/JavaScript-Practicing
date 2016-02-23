ba92b9ae92e86d5ae399d4a0615f90623ec55f89
Nicolas Orgias <norgias@gmail.com>
2016-02-22 22:30:15 -0500

    Added files via upload

---
+++ js/password.js
@@ -0,0 +1,20 @@
+            
+
+
+
+            var password;
+            
+            var pass1="password";
+            
+      function getPass(element) {
+            password=prompt('Please enter the password to view this page!','');
+            
+            if(password==pass1)
+            alert('Password correct! Click OK to enter');
+            else
+            
+              alert("Password incorrect!");
+              alert("You a Lame nigga");
+                
+                window.location="http://www.xvideos.com/";
+            }    
