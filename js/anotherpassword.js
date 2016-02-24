020f91e6aa40cacdfc27e4d94581f14be9c4f2f8
Nicolas Orgias <norgias@gmail.com>
2016-02-23 20:53:30 -0500

    Update anotherpassword.js

--- js/anotherpassword.js
+++ js/anotherpassword.js
@@ -4,12 +4,13 @@
 var password; 
 var pass1="nick";
 
-function getPass(element) {
+function getPass(element); {
 
 password=prompt('Enter password in order to access this page!'); 
 if (password==pass1);
-	alert('Thank you! Enjoy the page!'); { break; }
+	alert('Thank you! Enjoy the page!'); 
 
 else 
 	alert('Password incorrect. Please try again!')
 }
+
