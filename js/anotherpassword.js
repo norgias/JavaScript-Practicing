05de9b78e2e06711d43de517108e52012046ce68
Nicolas Orgias <norgias@gmail.com>
2016-02-23 20:55:59 -0500

    Update anotherpassword.js

--- js/anotherpassword.js
+++ js/anotherpassword.js
@@ -14,3 +14,15 @@ else
 	alert('Password incorrect. Please try again!')
 }
 
+function showProperties(element){
+  
+
+        document.getElementById('message').innerHTML = element.alt;
+        document.getElementById('message').style.backgroundColor = "yellow";
+}
+
+        function changeBack(element){
+
+        	document.getElementById('message').style.backgroundColor = "white";
+}
+
