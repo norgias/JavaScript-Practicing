d7fc08f99e2bed922d8239606f4ba0edd8c7f8fd
Nicolas Orgias <norgias@gmail.com>
2016-02-23 20:28:55 -0500

    Added files via upload

---
+++ js/anotherpassword.js
@@ -0,0 +1,15 @@
+
+
+
+var password 
+var pass1 = ('nick');
+
+function getPass(element);
+
+password = prompt('Enter password in order to access this page!'); {
+if (password = pass1);
+	alert('Thank you! Enjoy the page!'); { break; }
+
+else 
+	alert('Password incorrect. Please try again!')
+}
